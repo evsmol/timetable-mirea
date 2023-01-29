c36037afa5a878b8be782d315c79bcdee88a3862
Evgenii Smolentsev <evsmolentsev@gmail.com>
2023-01-29 20:44:53 +0300

    timedict to timelist

--- converter.py
+++ converter.py
@@ -3,8 +3,6 @@ from pandas import date_range
 
 from datetime import datetime
 
-from timedict_pattern import get_timedict_pattern
-
 # загрузка книги
 workbook = load_workbook('test.xlsx', read_only=True)
 worksheet = workbook.active
@@ -14,12 +12,13 @@ worksheet_list = list()
 for row in worksheet.rows:
     worksheet_list.append(row)
 
-# словарь с расписанием
-timedict = dict()
+# список с расписанием
+# (неделя, день, группа, пара, дисциплина, вид, преподаватель, аудитория)
+timelist = list()
 
 # список с датами
-start_date = datetime(2023, 2, 9)
-end_date = datetime(2023, 4, 23)
+start_date = datetime(2022, 9, 1)
+end_date = datetime(2023, 12, 22)
 
 dates = date_range(
     min(start_date, end_date),
@@ -35,9 +34,6 @@ for col in range(0, len(worksheet_list[1])):
     if cell is None or len(str(cell)) != 10 or len(cell.split('-')) != 3:
         continue
 
-    # шаблон словаря расписания
-    timedict[cell] = get_timedict_pattern()
-
     # получение расписания
     pair_number = None
     week_day = None
@@ -46,24 +42,40 @@ for col in range(0, len(worksheet_list[1])):
             pair_number = int(worksheet_list[row][1].value)
 
         if worksheet_list[row][0].value:
-            week_day = worksheet_list[row][0].value.capitalize()
+            match worksheet_list[row][0].value:
+                case 'ПОНЕДЕЛЬНИК':
+                    week_day = 1
+                case 'ВТОРНИК':
+                    week_day = 2
+                case 'СРЕДА':
+                    week_day = 3
+                case 'ЧЕТВЕРГ':
+                    week_day = 4
+                case 'ПЯТНИЦА':
+                    week_day = 5
+                case 'СУББОТА':
+                    week_day = 6
 
         if not worksheet_list[row][col].value:
             continue
 
-        week = 'Нечётная' if worksheet_list[row][col - 1].value == 'I' else \
-            'Чётная'
+        week = 1 if worksheet_list[row][col - 1].value == 'I' else 0
         discipline = worksheet_list[row][col].value
         activity_type = worksheet_list[row][col + 1].value
         teacher = worksheet_list[row][col + 2].value
         auditorium = worksheet_list[row][col + 3].value
 
-        timedict[cell][week][week_day][pair_number] = {
-            'Дисциплина': discipline,
-            'Вид занятий': activity_type,
-            'Преподаватель': teacher,
-            'Аудитория': auditorium
-        }
+        timelist.append((week, week_day,
+                         cell, pair_number,
+                         discipline, activity_type,
+                         teacher, auditorium))
 
-with open('test.json', 'w') as file:
-    file.write(str(timedict))
+# таблица с расписанием
+timetable = [['date', 'week', 'week_day',
+              'group', 'pair_number',
+              'discipline', 'activity_type',
+              'teacher', 'auditorium']]
+
+first_week = int(dates[0].split()[1])
+for row in timelist:
+    print(row)
