afc4e3d6db3e28a5633c9dd5dc544d1c76fcb74d
Evgenii Smolentsev <evsmolentsev@gmail.com>
2023-02-17 16:43:07 +0300

    add config

--- forms/MainForm.py
+++ forms/MainForm.py
@@ -1,8 +1,15 @@
 from PyQt6.QtWidgets import QMainWindow
 
+from config import VERSION
+
 
 class MainForm(QMainWindow):
     """Класс основного окна программы."""
 
     def __init__(self):
         super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(300, 300, 378, 40)
+        self.setWindowTitle(VERSION)
