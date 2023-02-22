892a65025951f2ee8c514af2d14123a9172e744e
Evgenii Smolentsev <evsmolentsev@gmail.com>
2023-02-23 01:00:44 +0300

    add toolbar

--- forms/MainForm.py
+++ forms/MainForm.py
@@ -1,7 +1,8 @@
-from PyQt6.QtCore import Qt
+from PyQt6.QtCore import Qt, QSize
 from PyQt6.QtWidgets import QMainWindow, QWidget
 from PyQt6.QtWidgets import QGridLayout, QStatusBar, QToolBar, \
     QLabel, QPushButton, QListWidget, QListWidgetItem
+from PyQt6.QtGui import QAction, QIcon
 
 from config import VERSION
 
@@ -18,6 +19,54 @@ class MainForm(QMainWindow):
     def initUI(self):
         self.setWindowTitle(f'Учебное расписание РТУ МИРЭА (v{VERSION})')
 
+        # создание панели инструментов
+        self.toolbar = QToolBar()
+        self.toolbar.setIconSize(QSize(24, 24))
+        self.toolbar.setMovable(False)
+        self.addToolBar(self.toolbar)
+
+        self.button_action_info = QAction(QIcon('image/info.png'),
+                                          'Информация', self)
+        self.button_action_info.triggered.connect(
+            self.toolbar_button_click_info
+        )
+        self.toolbar.addAction(self.button_action_info)
+
+        self.toolbar.addSeparator()
+
+        self.button_action_download = QAction(QIcon('image/download.png'),
+                                              'Загрузить расписание', self)
+        self.button_action_download.triggered.connect(
+            self.toolbar_button_click_download
+        )
+        self.toolbar.addAction(self.button_action_download)
+
+        self.toolbar.addSeparator()
+
+        self.button_action_left = QAction(QIcon('image/left.png'),
+                                          'Предыдущий месяц', self)
+        self.button_action_left.triggered.connect(
+            self.toolbar_button_click_left
+        )
+        self.toolbar.addAction(self.button_action_left)
+
+        self.button_action_right = QAction(QIcon('image/right.png'),
+                                           'Следующий месяц', self)
+        self.button_action_right.triggered.connect(
+            self.toolbar_button_click_right
+        )
+        self.toolbar.addAction(self.button_action_right)
+
+        self.toolbar.addSeparator()
+
+        self.button_action_error = QAction(QIcon('image/error.png'),
+                                           'Сообщить об ошибке', self)
+        self.button_action_error.triggered.connect(
+            self.toolbar_button_click_error
+        )
+        self.toolbar.addAction(self.button_action_error)
+
+        # создание виджетов
         self.layout = QGridLayout()
 
         self.list = QListWidget()
@@ -43,3 +92,18 @@ class MainForm(QMainWindow):
         widget = QWidget()
         widget.setLayout(self.layout)
         self.setCentralWidget(widget)
+
+    def toolbar_button_click_info(self):
+        pass
+
+    def toolbar_button_click_download(self):
+        pass
+
+    def toolbar_button_click_left(self):
+        pass
+
+    def toolbar_button_click_right(self):
+        pass
+
+    def toolbar_button_click_error(self):
+        pass
