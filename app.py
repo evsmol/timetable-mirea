a9a5a6c533bdd8704cd79b5da3eedfcf7a9fe02c
Evgenii Smolentsev <evsmolentsev@gmail.com>
2023-04-10 21:47:33 +0300

    redefine db and files

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
+import os
 import sys
 from PyQt6.QtWidgets import QApplication
 import qdarktheme
+
+import sqlite3
 
 from forms.MainForm import MainForm
 from data import db_session
@@ -11,6 +14,11 @@ def except_hook(cls, exception, traceback):
 
 
 if __name__ == '__main__':
+    if not os.path.isdir('files'):
+        os.mkdir('files')
+    if not os.path.isdir('db'):
+        os.mkdir('db')
+        sqlite3.connect('db/timetable.db')
     db_session.global_init('db/timetable.db')
 
     app = QApplication(sys.argv)
